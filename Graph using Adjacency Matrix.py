1442f2b32c3275bb1baf6bd52963c4ed6dadea65
aurnabnil66 <aurnab06@gmail.com>
2023-04-18 03:01:40 +0600

    graph using adj matrix not completed

---
+++ Graph using Adjacency Matrix.py
@@ -0,0 +1,24 @@
+vertices = []
+num_of_vertices = 0
+graph = []
+
+def add_vertex():
+    
+def add_edge():
+    
+
+# Adding vertices
+add_vertex("A")
+add_vertex("B")
+add_vertex("C")
+add_vertex("D")
+add_vertex("E")
+add_vertex("F")
+
+# Adding edges between specified vertices
+add_edge("A", "B", 2)
+add_edge("B", "C", 3)
+add_edge("C", "D", 1)
+add_edge("A", "E", 0)
+add_edge("E", "F", 4)
+add_edge("F", "D", 5)
